d723039cd446bb656ae1f233635f227069fa9d3c
yash <yashwanth.k@mooo.farm>
2024-01-04 21:05:13 +0530

    ping api test

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getPing(): any {
+    return {
+      server_time: `${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()}`,
+      server_name: 'notehub-api',
+      version: '1.0',
+    };
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+  // enabling swagger only on development
+  if (process.env.NODE_ENV === 'development') {
+    const config = new DocumentBuilder()
+      .setTitle('Notes API')
+      .setDescription('NoteHub API: Secure Note Management')
+      .setVersion('1.0')
+      .addTag('Notes Code System')
+      .build();
+    const document = SwaggerModule.createDocument(app, config, {
+      include: [],
+      deepScanRoutes: true,
+      operationIdFactory: (controllerKey: string, methodKey: string) =>
+        methodKey,
+    });
+    SwaggerModule.setup('api', app, document);
+  }
+  await app.listen(9000);
 }
 bootstrap();
